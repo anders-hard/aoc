028ff16e18c923f1043a56a8ea2e4ab43669e585
Anders Hård <Anders@meepo.se>
2022-12-04 21:13:24 +0100

    Solved day 4

---
+++ src/day4/index.ts
@@ -0,0 +1,40 @@
+import { Day } from "../day";
+import { splitByNewLine } from '../utils/stringUtils';
+
+class Day4 extends Day {
+
+  constructor(){
+    super(4);
+  }
+
+  solveForPartOne(input: string): number {
+    const pairs = splitByNewLine(input);
+    let sum = 0;
+    pairs.forEach(pair => {
+      const [elfOne, elfTwo] = pair.split(',');
+      const [oneStart, oneEnd] = elfOne.split('-');
+      const [twoStart, twoEnd] = elfTwo.split('-');
+      if ((parseInt(oneStart) <= parseInt(twoStart) && parseInt(oneEnd) >= parseInt(twoEnd)) ||
+          (parseInt(twoStart) <= parseInt(oneStart) && parseInt(twoEnd) >= parseInt(oneEnd))) {
+        sum++;
+      }
+    })
+    return sum;
+  }
+
+  solveForPartTwo(input: string): number {
+    const pairs = splitByNewLine(input);
+    let sum = 0;
+    pairs.forEach(pair => {
+      const [elfOne, elfTwo] = pair.split(',');
+      const [oneStart, oneEnd] = elfOne.split('-');
+      const [twoStart, twoEnd] = elfTwo.split('-');
+      if (!(parseInt(oneEnd) < parseInt(twoStart) || parseInt(twoEnd) < parseInt(oneStart))) {
+        sum++;
+      }
+    })
+    return sum;
+  }
+}
+
+export default new Day4;
