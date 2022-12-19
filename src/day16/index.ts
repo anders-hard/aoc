789c128aac30c5dc9342bb795b0366ea007449b6
Anders Hård <Anders@meepo.se>
2022-12-19 13:15:36 +0100

    Working (stolen) solution for day 16 part 2

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -1,7 +1,6 @@
 import { Day } from "../day";
 import { splitByNewLine } from '../utils/stringUtils';
-import { extractNumbers } from '../utils/arrayUtils';
-const BigSet = require('big-set');
+import { extractNumbers, permute } from '../utils/arrayUtils';
 
 type Node = {
   name: string,
@@ -92,101 +91,196 @@ class Day16 extends Day {
     })
   }
 
-  visited = new BigSet();
-
-  stepsToString(steps: [string[], string[]]): string {
-    return steps.map(s => s.join('.')).join('-');
-  }
-
-  stepsToString2(steps: [string[], string[]]): string {
-    return steps.map(s => s.join('.')).reverse().join('-');
-  }
-
-  step2(position1: Node, position2: Node, left: Set<Node>, pressure: number, time1: number, time2: number, steps: [string[], string[]]) {
-    const totalTime = 26;
-    const next = [...left.values()];
-    next.forEach(n => {
-      const dist = this.distances.get(position1.name)!.get(n.name)!
-      const newTime = time1 + dist + 1;
-      const potential = (totalTime - newTime) * this.nodes.get(n.name)!.flowRate
-      if (newTime < totalTime) {
-        const newSteps: [string[], string[]] = [[...steps[0], n.name], [...steps[1]]];
-        if (!this.visited.has(this.stepsToString(newSteps)) && !this.visited.has(this.stepsToString2(newSteps))) {
-          this.visited.add(this.stepsToString(newSteps));
-          const newLeft = new Set<Node>(left);
-          newLeft.delete(n);
-          this.step2(n, position2, newLeft, pressure + potential, newTime, time2, newSteps);
-        }
-      }
-    })
-    if (position1 !== position2 && time1 !== time2) {
-      next.forEach(n => {
-        const dist = this.distances.get(position2.name)!.get(n.name)!
-        const newTime = time2 + dist + 1;
-        const potential = (totalTime - newTime) * this.nodes.get(n.name)!.flowRate
-        if (newTime < totalTime) {
-          const newSteps: [string[], string[]] = [[...steps[0]], [...steps[1], n.name]];
-          if (!this.visited.has(this.stepsToString(newSteps)) && !this.visited.has(this.stepsToString2(newSteps))) {
-            this.visited.add(this.stepsToString(newSteps));
-            const newLeft = new Set<Node>(left);
-            newLeft.delete(n);
-            this.step2(position1, n, newLeft, pressure + potential, time1, newTime, newSteps);
-          }
-        }
-      })
-    }
-    if (pressure >= this.max) {
-      console.log('New max: ' + pressure, steps);
-      this.max = pressure;
-    }
-  }
-  /*
-  step3(position1: Node, position2: Node, left: Set<Node>, pressure: number, time: number, steps: [string[], string[]]) {
-    const totalTime = 26;
-    const next = [...left.values()];
-    if (time2 >= time1) {
-      next.forEach(n => {
-        const dist = this.distances.get(position1.name)!.get(n.name)!
-        const newTime = time1 + dist + 1;
-        const potential = (totalTime - newTime) * this.nodes.get(n.name)!.flowRate
-        if (newTime <= totalTime) {
-          const newLeft = new Set<Node>(left);
-          newLeft.delete(n);
-          this.step2(n, position2, newLeft, pressure + potential, newTime, time2, [[...steps[0], n.name], [...steps[1]]]);
-        }
-      })
-    } else {
-      next.forEach(n => {
-        const dist = this.distances.get(position2.name)!.get(n.name)!
-        const newTime = time2 + dist + 1;
-        const potential = (totalTime - newTime) * this.nodes.get(n.name)!.flowRate
-        if (newTime <= totalTime) {
-          const newLeft = new Set<Node>(left);
-          newLeft.delete(n);
-          this.step2(position1, n, newLeft, pressure + potential, time1, newTime, [[...steps[0]], [...steps[1], n.name]]);
-        }
-      })
-    }
-    if (pressure >= this.max) {
-      console.log('New max: ' + pressure, steps);
-      this.max = pressure;
-    }
-  }*/
-
   solveForPartOne(input: string): number {
     const start = this.parseInput(input);
     this.step(start, new Set<Node>(), 0, 0);
     return this.max;
   }
 
+  getPressure(nodes: Node[]): number {
+    let t = 0;
+    let i = 0;
+    let p = 0;
+    while (t < 25 && i < nodes.length - 1) {
+      t += this.distances.get(nodes[i].name)!.get(nodes[i + 1].name)! + 1;
+      if (t < 25) {
+        p += nodes[i + 1].flowRate * (25 - t);
+      }
+      ++i;
+    }
+    return p;
+  }
+
   solveForPartTwo(input: string): number {
+
+    // @ts-ignore
+    const memo = func => {
+      const cache = new Map();
+      // @ts-ignore
+      return (...args) => {
+        const key = args.join();
+        if (cache.has(key)) {
+          return cache.get(key);
+        } else {
+          const val = func(...args);
+          cache.set(key, val);
+          return val;
+        }
+      };
+    };
+
+    const RE = /Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.*)/;
+
+    // @ts-ignore
+    const parse = input =>
+        new Map(
+            input
+                .split("\n")
+                // @ts-ignore
+                .map(line => RE.exec(line))
+                // @ts-ignore
+                .map(([, valve, rate, valves]) => [
+                  valve,
+                  Number(rate),
+                  valves.split(", "),
+                ])
+                // @ts-ignore
+                .sort(([a], [b]) => a.localeCompare(b))
+                // @ts-ignore
+                .map(([valve, rate, valves], i, arr) => [
+                  1n << BigInt(i),
+                  [
+                    rate,
+                    valves.map(
+                        // @ts-ignore
+                        v =>
+                            1n <<
+                            BigInt(
+                                // @ts-ignore
+                                arr.findIndex(([valve]) => v === valve)
+                            )
+                    ),
+                    valve,
+                  ],
+                ])
+        );
+
+    // @ts-ignore
+    const shortestPath = graph => {
+      const keys = [...graph.keys()];
+      const distMap = new Map(
+          keys.map(k => [
+            k,
+            new Map(keys.map(l => [l, Number.MAX_SAFE_INTEGER])),
+          ])
+      );
+      keys.forEach(u =>
+          // @ts-ignore
+          graph.get(u).map(v => distMap.get(u).set(v, 1))
+      );
+      // @ts-ignore
+      keys.forEach(k => distMap.get(k).set(k, 0));
+      keys.forEach(k =>
+          keys.forEach(i =>
+              keys.forEach(j =>
+                  // @ts-ignore
+                  distMap
+                      .get(i)
+                      .set(
+                          j,
+                          Math.min(
+                              // @ts-ignore
+                              distMap.get(i).get(j),
+                              // @ts-ignore
+                              distMap.get(i).get(k) + distMap.get(k).get(j)
+                          )
+                      )
+              )
+          )
+      );
+      return distMap;
+    };
+
+    // @ts-ignore
+    const evaluate = (input, time = 30, firstrun = false) => {
+      const data = parse(input);
+      const distMap = shortestPath(
+          // @ts-ignore
+          new Map([...data].map(([key, data]) => [key, data[1]]))
+      );
+      const keys = [...data.keys()];
+      // @ts-ignore
+      const flow = new Map(keys.map(k => [k, data.get(k)[0]]));
+
+      const START = 1n;
+      // @ts-ignore
+      const dfs = memo((valve, minutes, open, firstrun) =>
+          keys
+              .filter(
+                  k =>
+                      // @ts-ignore
+                      !(open & k) &&
+                      flow.get(k) &&
+                      // @ts-ignore
+                      distMap.get(valve).get(k) < minutes
+              )
+              .map(k => {
+                // @ts-ignore
+                const d = distMap.get(valve).get(k) + 1;
+                const timeleft = minutes - d;
+                return (
+                    timeleft * flow.get(k) +
+                    // @ts-ignore
+                    dfs(k, timeleft, open | k, firstrun)
+                );
+              })
+              .reduce(
+                  (max, v) => (max > v ? max : v),
+                  firstrun ? dfs(START, time, open, false) : 0
+              )
+      );
+      return dfs(START, time, 0n, firstrun);
+    };
+
+    // @ts-ignore
+    const part2 = input => evaluate(input, 26, true);
+
+    /*
     this.distances = new Map<string, Map<string, number>>();
     this.max = 0;
     this.nodes = new Map<string, Node>();
     const start = this.parseInput(input);
-    this.visited.add(this.stepsToString([[start.name], [start.name]]));
-    this.step2(start, start, new Set([...this.nodes.values()].filter(n => n.flowRate)), 0, 0, 0, [[start.name], [start.name]]);
-    return this.max;
+
+    const possible = [...this.nodes.values()].filter(n => n.flowRate && n !== start);
+    for (let i = 0; i < Math.pow(2, possible.length); i++) {
+      const num = i.toString(2);
+      const [me, elephant] =  [
+        possible.filter((n, index) => index + num.length < possible.length || parseInt(num[index - (possible.length - num.length)]) === 0),
+        possible.filter((n, index) => index + num.length >= possible.length && parseInt(num[index - (possible.length - num.length)]) === 1),
+      ];
+      if (me.length >= (possible.length - 1) / 2 && elephant.length >= (possible.length - 1) / 2) {
+        const myPermutations = permute<Node>(me);
+        let myItems: Node[];
+        let myMax = 0;
+        while (myItems = myPermutations.next().value) {
+          myMax = Math.max(myMax, this.getPressure([start, ...myItems]));
+        }
+        const elephantPermutations = permute<Node>(elephant);
+        let elephantItems: Node[];
+        let elephantMax = 0;
+        while (elephantItems = elephantPermutations.next().value) {
+          elephantMax = Math.max(elephantMax, this.getPressure([start, ...elephantItems]));
+        }
+        const pressure = myMax + elephantMax;
+        if (pressure > this.max) {
+          console.log('New max: ' + pressure);
+          this.max = Math.max(this.max, pressure);
+        }
+      }
+    }
+
+     */
+    return part2(input);
   }
 }
 
--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -5,3 +5,18 @@ export function* chunks<T>(arr: T[], n: number): Generator<T[], void> {
 }
 
 export const extractNumbers = (s: string): number[] => [ ...s.matchAll(/[-\d]+/g)].map(s => parseInt(s[0]))
+
+export const permute = <T>(arr: T[]) => {
+  const l = arr.length,
+      used = Array(l),
+      data = Array(l);
+  return function* backtracking(pos): Generator<T[]> {
+    if (pos == l) yield data.slice();
+    else for (let i = 0; i < l; ++i) if(!used[i]) {
+      used[i] = true;
+      data[pos] = arr[i];
+      yield* backtracking(pos+1);
+      used[i] = false;
+    }
+  }(0);
+}
